456546d8fc230d04602c5d6d9820c572e5c6a925
Shrenik Parekh <shrenikp7@gmail.com>
2020-09-10 11:40:36 -0700

    Update mysqldump_condition.py

--- mysqldump_condition.py
+++ mysqldump_condition.py
@@ -1,5 +1,6 @@
 ####
-#### This is Python basic script to take dump of MySQL table with condition / dump selected partition of the table 
+#### This is Python basic script to take dump of MySQL table with condition / dump selected partition of the table.
+#### Intereseting item is to notice escape "\" to tell Python interpreter how to interpret double quote " and get into command line
 ####
 
 #!/usr/bin/python
@@ -15,8 +16,8 @@ DUMPFILE1='/mysql/dump/<FileName>.sql'
 DUMPFILE2='/mysql/dump/<FileName>.sql'
 CONDITION="<Date> between '20201001000000' and '20201101000000'"
 
-dumpdb1 = "mysqldump" + " " + "-u" + USER +" " +  "-p" + CODE + " " + "-S" + SOCKET + " " +  DB1 + " " + TABLE1 +" " + "--where=" + CONDITION + ">" +" " + DUMPFILE1
+dumpdb1 = "mysqldump" + " " + "-u" + USER +" " +  "-p" + CODE + " " + "-S" + SOCKET + " " +  DB1 + " " + TABLE1 +" " + "--where=" +"\""+ CONDITION +"\"" ">" +" " + DUMPFILE1
 os.system(dumpdb1)
 
-dumpdb2 = "mysqldump" + " " + "-u" + USER +" " +  "-p" + CODE + " " + "-S" + SOCKET + " " +  DB1 + " " + TABLE2 +" " + "--where=" + CONDITION + ">" +" " + DUMPFILE2
+dumpdb2 = "mysqldump" + " " + "-u" + USER +" " +  "-p" + CODE + " " + "-S" + SOCKET + " " +  DB1 + " " + TABLE2 +" " + "--where=" +"\""+ CONDITION +"\"" ">" +" " + DUMPFILE2
 os.system(dumpdb2)
