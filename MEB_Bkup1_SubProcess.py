c999476b18de0e060e86a35411007dc5d777730b
Shrenik Parekh <shrenikp7@gmail.com>
2021-03-08 17:56:35 -0800

    Update MEB_Bkup1_SubProcess.py

--- MEB_Bkup1_SubProcess.py
+++ MEB_Bkup1_SubProcess.py
@@ -41,4 +41,21 @@ if result == 2:
 else:
   cmd4="mailx -s 'MEB Backup FAILED ' " +  mail_list + "< "+ logfile
   p4 = subprocess.Popen(cmd4, stdin=None, stdout=subprocess.PIPE, shell=True)
+
+####
+#### Folwoing funciton remove files older than 7 days
+####
+def remove_files(dir_path, n):
+    all_files = os.listdir(dir_path)
+    now = time.time()
+    n_days = n * 86400
+    for f in all_files:
+        file_path = os.path.join(dir_path, f)
+        if not os.path.isfile(file_path):
+            continue
+        if os.stat(file_path).st_mtime < now - n_days:
+            os.remove(file_path)
+            print("Deleted ", f)
+
+remove_files(base_path, 7)
 # End of script
